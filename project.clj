aa3aff555c23ce66b1de2c91a2f33c1aaf963848
ogrisel <olivier.grisel@ensta.org>
2010-02-24 17:55:25 +0100

    update dependencies to released versions + add cascading-clojure dependency even though not used yet but still so very cool

--- project.clj
+++ project.clj
@@ -2,11 +2,12 @@
   :description "Utilities to build NLP corpus from Wikipedia / DBPedia dumps"
   :url "http://github.com/ogrisel/corpusmaker"
   :repositories {"info-bliki" "http://gwtwiki.googlecode.com/svn/maven-repository/"}
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
-                 [redis-clojure/redis-clojure "1.0-SNAPSHOT"]
-                 [stax/stax "1.2.0"]
-                 [info.bliki.wiki/bliki-core "3.0.13"]]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [redis-clojure "1.0-SNAPSHOT"]
+                 [stax "1.2.0"]
+                 [info.bliki.wiki/bliki-core "3.0.13"]
+                 [cascading-clojure "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [lein-javac "0.0.2-SNAPSHOT"]
                      [org.clojure/swank-clojure "1.0"]])
