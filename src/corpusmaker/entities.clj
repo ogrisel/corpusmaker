42be6e36c06cc250ffe602c11682b29e8a44a0c0
ogrisel <olivier.grisel@ensta.org>
2010-03-26 19:01:40 +0100

    utilities to parse dbpedia dumps

--- src/corpusmaker/entities.clj
+++ src/corpusmaker/entities.clj
@@ -11,7 +11,12 @@
 
 (ns corpusmaker.entities
   (:use
-     [clojure.contrib.duck-streams :only (read-lines)]))
+    [clojure.contrib.duck-streams :only (read-lines)])
+  (:require
+    (cascading.clojure [api :as c]))
+  (:import
+    cascading.operation.regex.RegexParser
+    cascading.pipe.Each))
 
 ;; Utilities to parse DBPedia N-TRIPLES and make it easier to
 ;; process them in cascading flows
@@ -20,18 +25,31 @@
 ;; RE to parse a line of a N-TRIPLES RDF stream to find a type relationship
 ;; http://www.w3.org/TR/rdf-testcases/#ntriples
 
-
 (def *wikipage-filename* "wikipage_en.nt")
-(def *type-pattern*
-  #"<([^<]+?)> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <([^<]+?)> \.")
-
 (def *instancetype-filename* "instancetype_en.nt")
-(def *page-pattern*
-  #"<([^<]+?)> <http://xmlns.com/foaf/0.1/page> <([^<]+?)> \.")
+(def *longabstract-filename* "longabstract_en.nt")
 
-(def *uri-uri-literal-pattern*
-  #"<([^<]+?)> <([^<]+?)> \"(.*)\"@(\w\w) \.")
+(def *uri-uri-uri-pattern* #"<([^<]+?)> <([^<]+?)> <([^<]+?)> \.")
+(def *uri-uri-literal-pattern* #"<([^<]+?)> <([^<]+?)> \"(.*)\"@(\w\w) \.")
 
 (def *owl-Thing* "http://www.w3.org/2002/07/owl#Thing")
+(def *rdf-type* "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+(def *foaf-page* "http://xmlns.com/foaf/0.1/page")
 
+(defn url-decode
+  [url]
+  (java.net.URLDecoder/decode url))
+
+(defn extract-property
+  "Extract entity propety value (object is a literal)"
+  {:fn> ["resource" "property" "value" "lang"]}
+  [line]
+  (let [[r p v l] (rest (re-find *uri-uri-literal-pattern* line))]
+    [(url-decode r) (url-decode p) (.replace v "\\\"" "\"") l]))
+
+(defn extract-relation
+  "Extract entities relationsips (subject predicate and object are URIs)"
+  {:fn> ["src-entity" "relation" "target-entity"]}
+  [line]
+  (map url-decode (rest (re-find *uri-uri-uri-pattern* line))))
 
--- test/corpusmaker/test_entities.clj
+++ test/corpusmaker/test_entities.clj
@@ -1,6 +1,44 @@
 (ns corpusmaker.test-entities
   (:use
-     clojure.test
-     corpusmaker.entities))
+    clojure.test
+    corpusmaker.entities
+    cascading.clojure.io)
+  (:require
+    [clojure.contrib.java-utils :as ju]
+    [clojure.contrib.duck-streams :as ds]
+    [cascading.clojure.api :as c]))
 
-(def *long-abstracts* "test/dbpedia_3.4_longabstract_en.nt")
+(def *long-abstract* "test/dbpedia_3.4_longabstract_en.nt")
+(def *instance-type* "test/dbpedia_3.4_instancetype_en.nt")
+
+(deftest extract-property-test
+  (let [triples (ds/read-lines (ju/file *long-abstract*))
+        results (map extract-property triples)]
+    (let [[resource property value lang] (first results)]
+      (is (= "http://dbpedia.org/resource/!!!" resource))
+      (is (= "http://dbpedia.org/property/abstract" property))
+      (is (= 321 (.length value)))
+      (is (= "!!! is a dance-punk band" (.substring value 0 24)))
+      (is (= "en" lang)))))
+
+(deftest extract-relation-test
+  (let [triples (ds/read-lines (ju/file *instance-type*))
+        results (map extract-relation triples)]
+    (let [[source relation target] (first results)]
+      (is (= "http://dbpedia.org/resource/!!!" source))
+      (is (= "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" relation))
+      (is (= "http://www.w3.org/2002/07/owl#Thing" target)))))
+
+(deftest entity-literal-parsing-test
+  (with-log-level :warn
+    (with-tmp-files [sink (temp-path "corpusmaker-test-sink")]
+      (let [flow (c/flow
+        {"abstracts" (c/lfs-tap (c/text-line "line") *long-abstract*)}
+        (c/lfs-tap (c/clojure-line) sink)
+        (->
+          (c/pipe "abstracts")
+          (c/map #'extract-property :< "line" :> ["resource" "value"])))]
+        (c/exec flow)
+        (let [results (map read-string (ds/read-lines (ju/file sink "part-00000")))]
+          (is (= 23 (.size results))))))))
+
