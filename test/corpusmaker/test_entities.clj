d3647da3248d49f7fda3e792b243c60efd037941
ogrisel <olivier.grisel@ensta.org>
2010-03-27 13:22:33 +0100

    testing inner join on dbpedia samples

--- test/corpusmaker/test_entities.clj
+++ test/corpusmaker/test_entities.clj
@@ -34,23 +34,41 @@
       (is (= "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" relation))
       (is (= "http://www.w3.org/2002/07/owl#Thing" target)))))
 
+(def agg-cons (c/agg cons ()))
+
 (deftest entity-literal-parsing-test
   (with-log-level :warn
     (with-tmp-files [sink (temp-path "corpusmaker-test-sink")]
-      (let [flow (c/flow
-        {"abstracts" (c/lfs-tap (c/text-line "line") *long-abstract*)}
-        (c/lfs-tap (c/text-line) sink)
-        (->
-          (c/pipe "abstracts")
-          (c/map #'extract-property
-            :< "line"
-            :fn> ["resource" "property" "value" "lang"]
-            :> ["resource" "value"])
-          (c/map #'serialize-tuple :< Fields/ALL :fn> "line" :> "line")))]
+
+      (let [p-abstracts ;; first pipe to extract abstracts data
+            (->
+              (c/pipe "abstracts")
+              (c/map #'extract-property
+                :< "line"
+                :fn> ["resource" "p_abstract" "abstract" "lang"]
+                :> ["resource" "abstract"]))
+            p-types ;; second pipe to extract typing info
+            (->
+              (c/pipe "types")
+              (c/map #'extract-relation
+                :< "line"
+                :fn> ["resource" "p_type" "type"]
+                :> ["resource" "type"]))
+            flow
+            (c/flow
+              {"abstracts" (c/lfs-tap (c/text-line "line") *long-abstract*)
+               "types" (c/lfs-tap (c/text-line "line") *instance-type*)}
+              (c/lfs-tap (c/text-line) sink)
+              (-> [p-abstracts p-types]
+                (c/inner-join
+                  [["resource"] ["resource"]]
+                  ["resource1" "abstract" "resource2" "type"])
+                (c/select ["resource1" "abstract" "type"])
+                (c/map #'serialize-tuple :< Fields/ALL :fn> "line" :> "line")))]
         (c/exec flow)
         (let [results (map read-string (ds/read-lines (ju/file sink "part-00000")))]
-          (is (= 23 (.size results)))
-          (let [[resource value] (first results)]
+          (is (= 54 (.size results)))
+          (let [[resource value type] (first results)]
             (is (= "http://dbpedia.org/resource/!!!" resource))
-            (is (= "!!! is a dance-punk band" (.substring value 0 24)))))))))
-
+            (is (= "!!! is a dance-punk band" (.substring value 0 24)))
+            (is (= "http://dbpedia.org/ontology/Band" type))))))))
